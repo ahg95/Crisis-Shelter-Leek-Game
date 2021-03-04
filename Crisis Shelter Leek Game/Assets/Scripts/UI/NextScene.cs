d846bd0db6187e83e3928da8d15fc9764251f250
OGdB <58260499+OGdB@users.noreply.github.com>
2021-03-04 21:24:11 +0100

    Main Menu & Pause Menu
    In the Pause Menu Scene, press escape to open the menu.

---
+++ Crisis Shelter Leek Game/Assets/Scripts/UI/NextScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public void GetNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Crisis Shelter Leek Game/Assets/Scripts/UI/OpenPauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class OpenPauseMenu : MonoBehaviour
+{
+    private bool isPaused = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Transform pauseMenu = transform.GetChild(0).transform;
+            isPaused = !isPaused;
+            Time.timeScale = isPaused ? 0 : 1;
+            pauseMenu.gameObject.SetActive(!pauseMenu.gameObject.activeSelf);
+
+            foreach(Transform child in pauseMenu)
+            {
+                child.gameObject.SetActive(false);
+            }
+            pauseMenu.GetChild(0).gameObject.SetActive(true);
+            pauseMenu.GetChild(1).gameObject.SetActive(true);
+        }
+    }
+}
