bb5cccf2a6fd1012fd09b679228debe9195ba2fd
OGdB <58260499+OGdB@users.noreply.github.com>
2021-03-09 21:15:40 +0100

    added cursor change

---
+++ Crisis Shelter Leek Game/Assets/CursorChange.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CursorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private Texture2D hoverCursor; 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(hoverCursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
+    }
+}
