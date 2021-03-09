614f1cd54a86f94f55d035c0225caf03d7459329
OGdB <58260499+OGdB@users.noreply.github.com>
2021-03-09 16:28:43 +0100

    testing a method for camera rotation and initiated a method for interaction

---
+++ Crisis Shelter Leek Game/Assets/Scripts/Interactable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Outline))]
+public class Interactable : MonoBehaviour
+{
+    private Outline outline;
+    [SerializeField]
+    private float outlineThickness = 8f;
+
+    private void Start()
+    {
+        outline = GetComponent<Outline>();
+        // Initiation
+        outline.OutlineColor = Color.yellow;
+        outline.OutlineWidth = outlineThickness;
+        outline.enabled = false;
+    }
+    private void OnMouseEnter()
+    {
+        outline.enabled = true;
+    }
+    private void OnMouseExit()
+    {
+        outline.enabled = false;
+    }
+}
