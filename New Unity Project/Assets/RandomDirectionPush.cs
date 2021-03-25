0f036345589130bd66923b84a89baf2c97e0ed1d
OGdB <58260499+OGdB@users.noreply.github.com>
2021-03-25 14:34:33 +0100

    last quick update

---
+++ New Unity Project/Assets/RandomDirectionPush.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RandomDirectionPush : MonoBehaviour
+{
+    private Rigidbody2D rgb;
+    [SerializeField] private float force = 25f;
+    private void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rgb.AddForce(Random.insideUnitCircle * force);
+        }
+    }
+}
