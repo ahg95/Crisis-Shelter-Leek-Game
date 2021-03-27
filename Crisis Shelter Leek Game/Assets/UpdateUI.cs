f936e0d7aa218ae17f445744d733bfa90bc18be0
OGdB <58260499+OGdB@users.noreply.github.com>
2021-03-27 11:49:50 +0100

    UI Canvas showing the amount of days and cost in the top-right and a class that updates it to the correct amount

---
+++ Crisis Shelter Leek Game/Assets/UpdateUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUI : MonoBehaviour
+{
+    [SerializeField] private Text days;
+    [SerializeField] private Text costs;
+
+    public void UpdateUIStats()
+    {
+        days.text = GlobalStats.newAmountOfDays.ToString();
+        costs.text = GlobalStats.newCost.ToString();
+    }
+}
