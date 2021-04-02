92c00fb870741847c646579f090b32d47b143358
OGdB <58260499+OGdB@users.noreply.github.com>
2021-04-02 12:29:45 +0200

    code cleanup (my own)

--- Crisis Shelter Leek Game/Assets/Scripts/Camera & Movement/SpawnOnFirstNode.cs
+++ Crisis Shelter Leek Game/Assets/Scripts/Camera & Movement/SpawnOnFirstNode.cs
@@ -15,10 +15,11 @@ public class SpawnOnFirstNode : MonoBehaviour
 
     private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
     {
-        Transform firstNode = GameObject.FindGameObjectWithTag("Respawn").transform;
+        GameObject firstNode = GameObject.FindGameObjectWithTag("Respawn");
+        firstNode.transform.GetComponent<MovementNode>().InteractWith();
 
-        gameObject.transform.position = firstNode.position;
-        gameObject.transform.rotation = firstNode.rotation;
+        gameObject.transform.position = firstNode.transform.transform.position;
+        gameObject.transform.rotation = firstNode.transform.transform.rotation;
     }
     
 }
