b8a0dcb8b9bd109f4087e74cd711fc449ba27e09
ahg95 <ansgar.glup@gmx.de>
2021-04-03 22:57:33 +0200

    Ansgars stuff

--- Crisis Shelter Leek Game/Assets/Scripts/Task System/AssignTaskToPlayer.cs
+++ Crisis Shelter Leek Game/Assets/Scripts/Task System/AssignTaskToPlayer.cs
@@ -9,6 +9,8 @@ public class AssignTaskToPlayer : MonoBehaviour
         currentTask.assignedTask = Instantiate(taskToAssign);
         // print("task " + taskToAssign.taskID + " assigned!");
 
+        GlobalStats.SaveTask(taskToAssign);
+
         // Update GUI
         GameObject currentAction = GameObject.Find("CurrentActionText");
         GameObject currentActionLocation = GameObject.Find("TaskLocation");
@@ -16,4 +18,5 @@ public class AssignTaskToPlayer : MonoBehaviour
         currentAction.GetComponent<TMPro.TextMeshProUGUI>().text = taskToAssign.description;
         currentActionLocation.GetComponent<TMPro.TextMeshProUGUI>().text = taskToAssign.location.ToString();
     }
+
 }
