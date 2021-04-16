9bdf49430709ab91bbe69ef9cdd0801bed710c03
DESKTOP-M0IPLA9\Master_CRAN <cristeaanastasia13@gmail.com>
2021-04-16 15:39:05 +0200

    personal testing with new dialogue system

---
+++ Crisis Shelter Leek Game/Assets/Scripts/NewDialogueSystem/AChoiceDialogue.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AChoiceDialogue : MonoBehaviour
+{
+    [SerializeField] Choice[] choice;
+}
---
+++ Crisis Shelter Leek Game/Assets/Scripts/NewDialogueSystem/Choice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName ="New Choice", menuName = "Choice")]
+public class Choice : ScriptableObject
+{
+    public UnityEvent consequence;
+    public Consequences[] specialConsequence;
+    public string description;
+    public int[] id;
+    public void ApplyConsequence()
+    {
+        GameObject.FindGameObjectWithTag("DCCM").GetComponent<DialogueChoicesConsequenceManager>().ApplyConsequenceToChoice(this);
+        Debug.Log(id);
+    }
+}
