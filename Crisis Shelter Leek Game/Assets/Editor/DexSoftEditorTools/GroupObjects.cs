83a5b278100ef4959a34b537a13b7a8db9ed8109
OGdB <58260499+OGdB@users.noreply.github.com>
2021-04-24 14:26:09 +0200

    replaced old Zienn location with new one + made a prefab in which the furniture is grouped

---
+++ Crisis Shelter Leek Game/Assets/Editor/DexSoftEditorTools/GroupObjects.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GroupObjects : EditorWindow
+{
+
+
+    [MenuItem("Tools/Group Selected/Pivot from first selected %g")]
+    private static void GroupSelected()
+    {
+        if (!Selection.activeTransform) return;
+        var go = new GameObject(Selection.activeTransform.name + " Group");
+        go.transform.position = Selection.activeTransform.position;
+        Undo.RegisterCreatedObjectUndo(go, "Group Selected");
+        go.transform.SetParent(Selection.activeTransform.parent, false);
+        foreach (var transform in Selection.transforms) Undo.SetTransformParent(transform, go.transform, "Group Selected");
+        Selection.activeGameObject = go;
+
+    }
+
+    [MenuItem("Tools/Group Selected/Pivot zeroed out #g")]
+    private static void GroupSelectedPivot()
+    {
+        if (!Selection.activeTransform) return;
+        var go = new GameObject(Selection.activeTransform.name + " Group");
+        Undo.RegisterCreatedObjectUndo(go, "Group Selected");
+        go.transform.SetParent(Selection.activeTransform.parent, false);
+        foreach (var transform in Selection.transforms) Undo.SetTransformParent(transform, go.transform, "Group Selected");
+        Selection.activeGameObject = go;
+
+    }
+
+
+}
