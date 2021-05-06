fe80d9030c92b32c026158c80db6257ce8a17c9f
ahg95 <ansgar.glup@gmx.de>
2021-05-06 14:31:21 +0200

    Stuff

--- Crisis Shelter Leek Game/Assets/Scripts/Dialogue system/ConversationSection.cs
+++ Crisis Shelter Leek Game/Assets/Scripts/Dialogue system/ConversationSection.cs
@@ -2,6 +2,10 @@
 public class ConversationSection : MonoBehaviour
 {
     public DialogueBoxContent[] dialogueBoxContent;
+
     [Space(10)]
     public Choice[] choicesAtEnd;
+
+    [Space(10)]
+    public ConversationSection followUpConversationIfNoChoicesPresent;
 }
