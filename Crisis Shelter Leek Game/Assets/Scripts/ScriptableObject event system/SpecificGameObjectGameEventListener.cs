9573dc139d35b0ba9446aed1b446cd73c592e515
ahg95 <ansgar.glup@gmx.de>
2021-05-06 15:16:49 +0200

    Added small Game event script

---
+++ Crisis Shelter Leek Game/Assets/Scripts/ScriptableObject event system/SpecificGameObjectGameEventListener.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecificGameObjectGameEventListener : GameObjectGameEventListener
+{
+    public GameObject gameObjectToListenFor;
+
+    public override void OnEventRaised(GameObject argument)
+    {
+        if (gameObjectToListenFor != null && ReferenceEquals(gameObjectToListenFor, argument))
+            base.OnEventRaised(argument);
+    }
+}
