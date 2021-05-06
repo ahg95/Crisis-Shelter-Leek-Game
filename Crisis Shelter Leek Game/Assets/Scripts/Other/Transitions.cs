34972f6d793f1d30f8f2e5c5edb2d4112884fa97
DESKTOP-M0IPLA9\Master_CRAN <cristeaanastasia13@gmail.com>
2021-05-06 16:06:40 +0200

    Changed a bit the system of scene transitioning with stats

---
+++ Crisis Shelter Leek Game/Assets/Scripts/Other/Transitions.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Transitions : MonoBehaviour
+{
+    [SerializeField] private Animator simpleTransition;//sets a simple transition that happens within the scene
+    [SerializeField] private Animator sceneTransition;//sets the transition on scene change
+    [SerializeField] private float sceneTransitionTime;//transition time for the scene transition
+
+    [SerializeField] private CanvasGroup stats;
+    [SerializeField] private float fadeInterval = 2f;//fade for the stats
+    private bool finishes;
+
+    private void Update()
+    {
+        finishes = gameObject.GetComponentInChildren<UpdateStats>().finished;
+    }
+
+
+    /// <summary>
+    /// Starts a scene change with transition
+    /// </summary>
+    /// <param name="name"> Tell the name of the scene </param>
+    public void LoadSceneTransition(string name)
+    {
+        StartCoroutine(LoadScene(name));
+    }
+
+    /// <summary>
+    /// Starts a simple transition with statistics that can change scene
+    /// </summary>
+    /// <param name="name">Tell the name of the scene to switch to </param>
+    /// <param name="switchScene">Tells whether to switch the scene or not</param>
+    public void SimpleTransitionStats(string name)
+    {
+        StartCoroutine(TransitionWithStats(true, fadeInterval, true, name));
+    }
+
+
+    /// <summary>
+    /// Switches scenes with transitions
+    /// </summary>
+    /// <param name="sceneName">Tells the name of the scene to switch to</param> 
+    /// <returns></returns>
+    IEnumerator LoadScene(string sceneName)
+    {
+        yield return new WaitForSeconds(1f);
+
+        simpleTransition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(sceneTransitionTime);
+
+        SceneManager.LoadScene(sceneName);
+    }
+
+    /// <summary>
+    /// Creates a transition with stats
+    /// </summary>
+    /// <param name="showStats"> Tells if you want display stats or not </param>
+    /// <param name="addedFade"> Tells with what interval the stats alpha show</param>
+    /// <param name="switchScene"> Tell if you want to switch scenes </param>
+    /// <param name="sceneName"> Tell what scene to switch to </param>
+    /// <returns></returns>
+    public IEnumerator TransitionWithStats(bool showStats, float addedFade, bool switchScene, string sceneName)
+    {
+        float fadeAmount;
+        simpleTransition.SetTrigger("Start");//starts the transition
+
+        if (showStats)
+        {
+            yield return new WaitForSeconds(1);
+
+            while (stats.alpha < 1) // under 1
+            {
+                fadeAmount = stats.alpha + (addedFade * Time.deltaTime); // add
+
+                stats.alpha = fadeAmount;
+                yield return null;
+            }
+        }
+        yield return new WaitUntil(() => finishes);//waits until the stats have finished showing 
+        //yield return new WaitUntil(() => Input.GetMouseButtonDown(0));//waits until the player clicked the mouse 
+        yield return new WaitForSeconds(1.5f);//waits for a bit more sec before switching scenes
+
+        if (showStats)
+        {
+            while (stats.alpha > 0) // above 0
+            {
+                fadeAmount = stats.alpha - (addedFade * Time.deltaTime); // substract
+                stats.alpha = fadeAmount;
+                yield return null;
+            }
+        }
+        simpleTransition.SetTrigger("End");//ends the transition
+
+        if (switchScene)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
