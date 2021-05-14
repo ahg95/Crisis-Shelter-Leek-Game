9958144d490c51396be230575d826dea1f6ad65d
ahg95 <ansgar.glup@gmx.de>
2021-05-14 19:41:35 +0200

    changed scripts to my original game event system

--- Crisis Shelter Leek Game/Assets/Scripts/ScriptableObject event system/SpecificGameObjectGameEventListener.cs
+++ Crisis Shelter Leek Game/Assets/Scripts/ScriptableObject event system/SpecificGameObjectGameEventListener.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class SpecificGameObjectGameEventListener : GameObjectGameEventListener
+public class SpecificGameObjectGameEventListener : GameEventListener
 {
     public GameObject gameObjectToListenFor;
 
-    public override void OnEventRaised(GameObject argument)
+    [SerializeField]
+    public GameObjectGameEvent GameEvent;
+
+    [SerializeField]
+    public UnityEvent<GameObject> Response;
+
+    public void OnEventRaised(GameObject argument)
     {
         if (gameObjectToListenFor != null && ReferenceEquals(gameObjectToListenFor, argument))
-            base.OnEventRaised(argument);
+            Response.Invoke(argument);
     }
+
+
+
+    private void OnEnable() => GameEvent.AddListener(this);
+
+    private void OnDisable() => GameEvent.RemoveListener(this);
 }
