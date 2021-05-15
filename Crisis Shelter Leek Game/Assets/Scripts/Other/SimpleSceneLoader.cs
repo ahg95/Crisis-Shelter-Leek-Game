690fb49a49ca306cfe060fe2c8a13a060be5e5e3
ahg95 <ansgar.glup@gmx.de>
2021-05-15 14:49:49 +0200

    added simple sceneloader

---
+++ Crisis Shelter Leek Game/Assets/Scripts/Other/SimpleSceneLoader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleSceneLoader : MonoBehaviour
+{
+    public enum GameScene {
+        Wender,
+        WenderRoom,
+        WenderRoomDaisy,
+        Municipality
+    }
+
+    public void LoadScene(GameScene scene)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+
+    private string GetSceneNameForGameScene(GameScene scene)
+    {
+        string sceneName;
+
+        switch (scene)
+        {
+            case GameScene.Wender:
+                sceneName = "Wender";
+                break;
+            case GameScene.WenderRoom:
+                sceneName = "Wender room";
+                break;
+            case GameScene.WenderRoomDaisy:
+                sceneName = "Wender room with Daisy";
+                break;
+            case GameScene.Municipality:
+                sceneName = "Municipality";
+                break;
+            default:
+                sceneName = scene.ToString();
+                break;
+        }
+
+        return sceneName;
+    }
+
+}
