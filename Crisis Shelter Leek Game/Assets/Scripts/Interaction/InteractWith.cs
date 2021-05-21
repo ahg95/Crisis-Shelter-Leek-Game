d3edca49daa7ca91f06b3a105c8925441350fc7d
OGdB <58260499+OGdB@users.noreply.github.com>
2021-05-21 17:46:59 +0200

    Make Main Wender Scene

--- Crisis Shelter Leek Game/Assets/Scripts/Interaction/InteractWith.cs
+++ Crisis Shelter Leek Game/Assets/Scripts/Interaction/InteractWith.cs
@@ -143,6 +143,7 @@ public class InteractWith : MonoBehaviour
             }
 
             print("zoomed out!");
+            navComponent.enabled = true;
             zoomedIn = false;
             interactableInteractedWith.isSelected = false;
             interactableInteractedWith = null;
