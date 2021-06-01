bfb21c8061a70ea936786c04fc2300878eab7531
OGdB <58260499+OGdB@users.noreply.github.com>
2021-06-01 16:41:08 +0200

    Implementation of animated character (Coach)

---
+++ Crisis Shelter Leek Game/Assets/Scripts/MoveToShowRoom.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToShowRoom : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent = null;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Transform destination;
+    [SerializeField] private Transitions transitions;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        StartCoroutine(WaitForDestinationReached(destination));
+    }
+
+    private IEnumerator WaitForDestinationReached(Transform destination)
+    {
+        agent.SetDestination(destination.position);
+        animator.SetBool("isWalking", true);
+
+        if (agent.pathPending) // need to check for this, otherwise the while loop  might return true, because the path hadn't been calculated yet.
+        {
+            //print("Path Pending");
+            yield return null;
+        }
+        while (agent.remainingDistance > 0.1f)
+        {
+            //print("moving towards destination");
+            yield return new WaitForFixedUpdate();
+        }
+
+        animator.SetBool("isWalking", false);
+        transitions.LoadSimpleSceneTransition("Bedroom Wender With Coach");
+        // When destination reached, switch scene.
+    }
+
+}
