3569205995b18554facecab28674a7cf5938bcff
OGdB <58260499+OGdB@users.noreply.github.com>
2021-06-07 18:56:17 +0200

    Disable On Task Bedroom

---
+++ Crisis Shelter Leek Game/Assets/Scripts/Other/DisableOnTask.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Interactable))]
+[DisallowMultipleComponent]
+public class DisableOnTask : MonoBehaviour
+{
+    [SerializeField] private TaskJourney taskJourney;
+    [SerializeField] private Task[] tasks;
+    [Header("Settings")]
+    [SerializeField] private bool disableInteraction = false;
+
+    private void Start()
+    {
+        if (disableInteraction)
+        {
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                Task task = tasks[i];
+                if (taskJourney.assignedTask == task)
+                {
+                    GetComponent<Interactable>().enabled = false;
+                    GetComponent<Outline>().enabled = false;
+                    GetComponent<Collider>().enabled = false;
+                    gameObject.layer = 0;
+                }
+            }
+        }
+    }
+}
