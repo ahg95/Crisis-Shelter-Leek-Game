3dbaf0ae3c2dd5f541a07f3f9412af95ad8f6b13
OGdB <58260499+OGdB@users.noreply.github.com>
2021-06-08 23:54:39 +0200

    Made Drawers and Cabinet in bedroom interactable

---
+++ Crisis Shelter Leek Game/Assets/Scripts/Interaction/OpenCabinet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Interactable))]
+public class OpenCabinet : MonoBehaviour
+{
+    [Range(0.5f, 2f)]
+    [SerializeField] private float rotationSpeed = 0.75f;
+    [SerializeField] private float targetAngle = -90f;
+    private bool rotating = false;
+
+    private void OnEnable()
+    {
+        GetComponent<Interactable>().onInteraction.AddListener(RotateCabinet);
+    }
+    public void RotateCabinet()
+    {
+        if (!rotating)
+        {
+            StartCoroutine(Rotate(targetAngle));
+            targetAngle *= -1;
+        }
+    }
+
+    private IEnumerator Rotate(float targetAngle)
+    {
+        rotating = true;
+
+        float startTime = Time.time;
+        Quaternion startRotation = transform.localRotation;
+        Quaternion targetRotation = Quaternion.AngleAxis(targetAngle, Vector3.up) * transform.localRotation;
+
+        while (transform.localRotation != targetRotation)
+        {
+            float timeSinceStarted = Time.time - startTime;
+            float progress = timeSinceStarted / rotationSpeed;
+
+            transform.localRotation = Quaternion.Slerp(startRotation, targetRotation, progress);
+
+            yield return null;
+        }
+
+        rotating = false;
+    }
+}
