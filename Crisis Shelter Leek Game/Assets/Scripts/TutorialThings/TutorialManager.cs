b926d2b518345e1aa5dc837fbcef2f0eb87bd702
DESKTOP-M0IPLA9\Master_CRAN <cristeaanastasia13@gmail.com>
2021-06-15 14:06:57 +0200

    Making new test scene for tutorial

--- Crisis Shelter Leek Game/Assets/Scripts/TutorialThings/TutorialManager.cs
+++ Crisis Shelter Leek Game/Assets/Scripts/TutorialThings/TutorialManager.cs
@@ -7,12 +7,11 @@ using UnityEngine.Animations;
 public class TutorialManager : MonoBehaviour
 {
     #region Variables
-    [SerializeField]private GameObject[] popUps; //the key tutorial objects
+    [SerializeField] private GameObject[] popUps; //the key tutorial objects
     [SerializeField] private Animator[] rotationAnim; //popUp Animations
     [SerializeField] private float[] waitTime; //how much you have to wait until you disable the object again(disable after the end animation has finished)
 
     [SerializeField] private int popUpId; // the id for every tutorial part
-    private int n = 0;
 
     //tutorial parts checks
     [SerializeField] private bool[] tutorialParts;
@@ -27,11 +26,11 @@ public class TutorialManager : MonoBehaviour
     //tutorial components
     private OnButtonHover[] cameraRot;
     private Navigation nav;
-    private Interactable[] camZoom;
-
-    [SerializeField]private List<Interactable> targetedCamZoom;
+    private InteractWith camZoom;
 
     #endregion
+
+    #region Functions
     void Start()
     {
         skipTutorialButton = GameObject.Find("SkipTutorialButton");
@@ -47,19 +46,44 @@ public class TutorialManager : MonoBehaviour
         dialogManager = gameObject.GetComponentInChildren<DialogManager>();
         cameraRot = GameObject.FindWithTag("Player").GetComponentsInChildren<OnButtonHover>();
         nav = GameObject.FindWithTag("Player").GetComponent<Navigation>();
-        camZoom = GameObject.FindObjectsOfType<Interactable>();
+        camZoom = GameObject.FindGameObjectWithTag("Player").GetComponent<InteractWith>();
     }
 
-/*    private void Update()
+    public void StartTutorial()
+    {
+        tutorialActive = true;
+        skipTutorialButton.SetActive(true);
+        StartCoroutine("TutorialStart");
+    }
+
+    public void SkipTutorial()
+    {
+        popUpId = popUps.Length;
+        dialogManager.EndDialogue();
+        skipTutorialButton.SetActive(false);
+        StopCoroutine("TutorialStart");
+    }
+
+    /// <summary>
+    /// This function will be called when the player succeded at accomplishing a tutorial part
+    /// </summary>
+    /// <param name="partNr"> The tutorial part id that you want to accomplish </param>
+    public void CompleteTutorialPart(int partNr)
     {
         if (tutorialActive)
         {
-            StartTutorial();
+            tutorialParts[partNr] = true;
         }
-    }*/
+    }
+    /// <summary>
+    /// This function calls the coroutine that checks if some parts of the tutorial have been completed
+    /// </summary>
+    public void CheckTutorial()
+    {
+        StartCoroutine("CheckIfCompletedTutorialParts");
+    }
 
-    #region TutorialChecks
-    //Check is the player is hovering over any rotation buttons
+    //Check is the player is hovering over any rotation buttons(first tutorial part)
     private bool CheckButtonHover()
     {
         bool hovering = false;
@@ -72,129 +96,61 @@ public class TutorialManager : MonoBehaviour
         }
         return hovering;
     }
-
-    //Check if player is zooming in on an inspectable
-    private bool CheckZoomIn()
-    {
-        bool zooming = false;
-        for (int i = 0; i < camZoom.Length; i++)
-        {
-            if (camZoom[i].zoomedInOn && camZoom[i].isSelected)
-            {
-                zooming = true;
-                targetedCamZoom.Add(camZoom[i]);
-            }
-        }
-        return zooming;
-    }
-
-    //Check if player is zooming out on an inspectable
-    private bool CheckZoomOut()
-    {
-        bool zoomingOut = false;
-
-        if (tutorialParts[3] && !targetedCamZoom[0].zoomedInOn && !targetedCamZoom[0].isSelected)
-        {
-            zoomingOut = true;
-            targetedCamZoom.Clear();
-        }
-        return zoomingOut;
-    }
     #endregion
-    public void CheckTutorial()
+
+    #region Coroutines
+
+    /// <summary>
+    /// As long as the tutorial started and hasn't finished keep looping through the tutorial parts
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator TutorialStart()
     {
-        StartCoroutine("CheckForTutorialParts");
-    }
-    IEnumerator CheckForTutorialParts()
-    {
-        yield return new WaitUntil(() => CheckButtonHover());
-        TutorialPartAchieved(0);
-
-        //Check if player is pressing on the ground
-        yield return new WaitUntil(() => nav.arrow.activeSelf && Input.GetMouseButton(0));
-        TutorialPartAchieved(1);
-
-        yield return new WaitUntil(() => CheckZoomIn());
-        TutorialPartAchieved(3);
-
-        yield return new WaitUntil(() => CheckZoomOut());
-        TutorialPartAchieved(4);
-
-    }
-
-    public void StartTutorial()
-    {
-        //StartCoroutine("TutorialStart");
-        tutorialActive = true;
-        skipTutorialButton.SetActive(true);
-        for (int i = 0; i < popUps.Length; i++)
+        for (int i = 0; i < popUps.Length; i++)//when a tutorial part is showing the other parts are inactive
         {
             if (i == popUpId) { popUps[i].SetActive(true); }
             else { popUps[i].SetActive(false); }
         }
 
-        if (popUpId == n && n < popUps.Length){
-            if (tutorialParts[n])
+        if (popUpId < popUps.Length)
+        {
+            if (tutorialParts[popUpId])//whenever you accomplish a tutorial part move on to the next one
             {
-                rotationAnim[n].SetBool("animOff", true);
-                if (waitTime[n] <= 0)
-                {
-                    n++;
-                    popUpId = n;
-                    dialogManager.DisplayNextSentence();
-                }
-                else
-                {
-                    waitTime[n] -= Time.deltaTime;
-                }
+                rotationAnim[popUpId].SetBool("animOff", true);
+
+                yield return new WaitForSeconds(waitTime[popUpId]);
+                popUpId++;
+                dialogManager.DisplayNextSentence();
             }
         }
         else if (popUpId == popUps.Length)
         {
             finishDialogueButton.SetActive(true);
             skipTutorialButton.SetActive(false);
+            StopCoroutine("TutorialStart");
         }
-    }
-    IEnumerator TutorialStart()
-    {
-        tutorialActive = true;
-        skipTutorialButton.SetActive(true);
 
         yield return null;
-    }
-    //this function will skip the tutorial 
-    public void SkipTutorial()
-    {
-        popUpId = popUps.Length;
-        dialogManager.EndDialogue();
-        skipTutorialButton.SetActive(false);
+        StartCoroutine("TutorialStart");
     }
 
-    /// <summary>
-    /// This function will be called when the player succeded at accomplishing a tutorial part
-    /// </summary>
-    /// <param name="partNr"> The order of the tutorial part that you wish to accomplish from the array </param>
-    public void TutorialPartAchieved(int partNr)
+    IEnumerator CheckIfCompletedTutorialParts()
     {
-        if (tutorialActive)
-        {
-            tutorialParts[partNr] = true;
-        }
+        yield return new WaitUntil(() => CheckButtonHover());
+        CompleteTutorialPart(0);
+
+        //Check if player is pressing on the ground
+        yield return new WaitUntil(() => nav.arrow.activeSelf && Input.GetMouseButton(0));
+        CompleteTutorialPart(1);
+
+        yield return new WaitUntil(() => camZoom.isZoomedIn);
+        CompleteTutorialPart(3);
+
+        yield return new WaitForSeconds(1.5f);
+        yield return new WaitUntil(() => !camZoom.isZoomedIn);
+        CompleteTutorialPart(4);
     }
+
+    #endregion
 
 }
-
-
-
-//have arrows for moving to the left or right on the screen
-//a text pop-up for moving to arrow => which is already highlighted
-//an arrow pointing to an object you can interact with 
-//a highlight over the task ui with a popup saying what's it for
-
-
-//get the onbutton hover and check if starttime Count is true
-//check if arrow is visible and you clicked
-
-
-//look at pamphlet
-//look at task
