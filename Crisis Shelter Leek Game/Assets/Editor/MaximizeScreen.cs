819d49c7acc9e352a0e920bb769a3a1993d03cfe
OGdB <58260499+OGdB@users.noreply.github.com>
2021-06-16 10:33:44 +0200

    maxmize screen editor script

---
+++ Crisis Shelter Leek Game/Assets/Editor/MaximizeScreen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class MaximizeScreen : EditorWindow
+{
+    [MenuItem("Tools/Maximize/Maximize Screen _escape")]
+    private static void MaxScreen()
+    {
+        EditorWindow.focusedWindow.maximized = !EditorWindow.focusedWindow.maximized;
+    }
+}
